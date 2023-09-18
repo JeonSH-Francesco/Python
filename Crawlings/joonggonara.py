db3b813752929160f68cf5cfee761171b49a922d
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-09-18 10:57:27 +0900

    Update joonggonara.py

--- Crawlings/joonggonara.py
+++ Crawlings/joonggonara.py
@@ -22,7 +22,9 @@ for li in sidebar.find_elements(By.TAG_NAME,"li"):
 
 cafe_main_frame=browser.find_element(By.ID,"cafe_main")
 browser.switch_to.frame(cafe_main_frame)
+#"cafe_main" ID를 가진 iframe 요소를 식별하고 WebDriver의 포커스를 이 iframe으로 전환합니다. 이로써 이후 작업이 iframe 내에서 수행됩니다..
 
+#iframe 내에서 <td> 요소의 수를 계산하고 출력한 다음 각 <td> 요소의 텍스트 내용을 반복하여 출력합니다.
 print(len(browser.find_elements(By.TAG_NAME,"td")))
 for td in browser.find_elements(By.TAG_NAME,"td"):
     print(td.text)
