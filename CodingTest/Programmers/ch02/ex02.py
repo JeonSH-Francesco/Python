68f995f71e21d62228ca71d4cf34e55326e2921d
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-09-30 14:03:03 +0900

    Create ex02.py

---
+++ CodingTest/Programmers/ch02/ex02.py
@@ -0,0 +1,13 @@
+print('Python is awesome'[3::2])
+
+data=[[(0,1),(2,3),(4,5)],[(6,7),(8,9),(0,1)]]
+print(data[1:][0][::2])
+
+data=[]
+for i in range(1,11):
+    data.append(i)
+print(data)
+
+data1=[i for i in range(1,11)]
+print(data1)
+
