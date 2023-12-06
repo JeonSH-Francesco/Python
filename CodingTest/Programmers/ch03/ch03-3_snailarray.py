a023b8d270bc97b1104a92a9020ba3195ad05a47
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-12-06 18:52:12 +0900

    Update ch03-3_snailarray.py

--- CodingTest/Programmers/ch03/ch03-3_snailarray.py
+++ CodingTest/Programmers/ch03/ch03-3_snailarray.py
@@ -1,49 +1,22 @@
 def solution(n):
-    res = [[0] * i for i in range(1, n+1)]
-    # n값을 기반으로 2차원 리스트를 생성하고 초기값을 0으로 설정
-    # 2차원 리스트 각 행의 길이는 1부터 n까지 순차적으로 증가
-    # n=4 인 경우
-    #res = [
-    #[0],        i = 1
-    #[0, 0],     i = 2
-    #[0, 0, 0],  i = 3
-    #[0, 0, 0, 0]  i = 4
-    # ]
-
-    y, x = -1, 0
-    # 현재 위치를 나타내는 변수로 y=-1, x=0으로 세팅
+    res = [[0] * i for i in range(1, n + 1)]
+    x, y = -1, 0
     num = 1
-    # 배열에 채워질 숫자를 나타내는 변수로 초기값 1로 세팅
 
     for i in range(n):
         for _ in range(i, n):
             angle = i % 3
-            # 현재 패턴에서의 이동방향 결정하는 angle 변수
-            # 순서대로 아래 -> 오른쪽 -> 위 (반시계 나선형)
-
-            # 아래인 경우
             if angle == 0:
-                y += 1
-                print("i : %d angle: %d x: %d y: %d" % (i, angle, x, y))
-                
-            # 오른쪽인 경우
-            elif angle == 1:
                 x += 1
-                print("i : %d angle: %d x: %d y: %d" % (i, angle, x, y))
-                
-            # 왼쪽, 대각선인 경우
+            elif angle == 1:
+                y += 1
             elif angle == 2:
-                y -= 1
                 x -= 1
-                print("i : %d angle: %d x: %d y: %d" % (i, angle, x, y))
-                
-            # 최종 결과 생성
-            res[y][x] = num
-            print("i: %d res[%d][%d]: %d" % (i, y, x, num))
+                y -= 1
+            res[x][y] = num
+            print("i: %d res[%d][%d]: %d" % (i, x, y, num))
             print("\n")
             num += 1
-
-    return [i for j in res for i in j]
     #for j in res : res 리스트의 각 원소 j에 대해 반복
     #res는 2차원 리스트 이므로 각 j는 하위 리스트를 나타낸다.
     #for i in j : 각 하위 리스트 j의 원소 i에 대해 반복
@@ -52,16 +25,16 @@ def solution(n):
     
     #원리를 파악해 보길 바란다. 3차원 리스트 인 경우
     #return [i for j in three_dim_list for k in j for i in k ]
-    
+    return [i for j in res for i in j]
+
 # n이 4, 5, 6인 경우의 출력 결과
 output_4 = solution(4)
 output_5 = solution(5)
 output_6 = solution(6)
 
-print(output_4) 
+print(output_4)
 print(output_5)
 print(output_6)
-
 '''
 출력 결과:
 [1, 2, 9, 3, 10, 8, 4, 5, 6, 7]
